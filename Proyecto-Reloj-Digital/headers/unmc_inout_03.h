7ff4ebdcdc9c8d12737a67141c1f26f2476288d2
ferGainey <fernandogainey@hotmail.com>
2017-09-11 16:47:04 -0300

    comenzamos con el reloj digital

---
+++ Proyecto-Reloj-Digital/headers/unmc_inout_03.h
@@ -0,0 +1,22 @@
+/*********************************************************************
+ *
+ *                Unimic XC8  Version 1.1   02 November 2015
+ *
+ *********************************************************************
+ * FileName:        unmc_config_03.h
+ * Dependencies:
+ * Processor:       PIC18F26J50
+ * Compiler:        XC8 1.34
+ * Company:         Microchip Technology, Inc.
+ *
+ *********************************************************************/
+
+///** S W I T C H *****************************************************/
+#define switch_Right    PORTAbits.RA2
+#define switch_Up       PORTAbits.RA3
+#define switch_Center   PORTAbits.RA5
+#define switch_Down     PORTAbits.RA6
+#define switch_Left     PORTAbits.RA7
+
+/********************************************************************/
+/********************************************************************/
